0161ca4b357ce91d1a2cb0d2f2ac098356613732
odilf <odysseas.maheras@gmail.com>
2025-05-23 13:34:35 +0200

    fix: add iter to fish script

--- bench.fish
+++ bench.fish
@@ -1,3 +1,6 @@
+argparse i/iteration -- $argv
+or return
+
 set -x RUST_BACKTRACE 1
 set -x RUST_LOG debug
 
@@ -5,33 +8,27 @@ nix develop .#z3-noodler --command fish -c "
 cargo run --release -- get-solutions
 cargo run --release -- get-solutions
 
-cargo run --release -- run z3-noodler 0.0
-cargo run --release -- run z3-noodler 0.5
-cargo run --release -- run z3-noodler 0.9
+cargo run --release -- run z3-noodler 0.0 --iteration $argv[1]
+cargo run --release -- run z3-noodler 0.9 --iteration $argv[1]
 "
 
 nix develop .#z3 --command fish -c "
-cargo run --release -- run z3 0.0
-cargo run --release -- run z3 0.5
-cargo run --release -- run z3 0.9
+cargo run --release -- run z3 0.0 --iteration $argv[1]
+cargo run --release -- run z3 0.9 --iteration $argv[1]
 "
 
 nix develop .#z3-noodler --command fish -c "
-cargo run --release -- run z3-noodler 0.0
-cargo run --release -- run z3-noodler 0.5
-cargo run --release -- run z3-noodler 0.9
+cargo run --release -- run z3-noodler 0.0 --iteration $argsv[1]
+cargo run --release -- run z3-noodler 0.9 --iteration $argsv[1]
 
-cargo run --release -- run z3-noodler 0.0
-cargo run --release -- run z3-noodler 0.5
-cargo run --release -- run z3-noodler 0.9
+cargo run --release -- run z3-noodler 0.0 --iteration $argv[1]
+cargo run --release -- run z3-noodler 0.9 --iteration $argv[1]
 "
 
 nix develop .#z3 --command fish -c "
-cargo run --release -- run z3 0.0
-cargo run --release -- run z3 0.5
-cargo run --release -- run z3 0.9
+cargo run --release -- run z3 0.0 --iteration $argv[1]
+cargo run --release -- run z3 0.9 --iteration $argv[1]
 
-cargo run --release -- run z3 0.0
-cargo run --release -- run z3 0.5
-cargo run --release -- run z3 0.9
+cargo run --release -- run z3 0.0 --iteration $argv[1]
+cargo run --release -- run z3 0.9 --iteration $argv[1]
 "
